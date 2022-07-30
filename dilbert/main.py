417673b65c48669d569936d513751d9076f1dddc
Alphacode18 <skharban@purdue.edu>
2022-07-30 19:10:45 +0530

    feat: adds message parsing for "doi" containing links

--- dilbert/main.py
+++ dilbert/main.py
@@ -2,6 +2,7 @@ import logging
 import os
 import re
 
+import validators
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -11,24 +12,20 @@ load_dotenv()
 SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
-app = App(token=SLACK_BOT_TOKEN, name="Joke Bot")
+app = App(token=SLACK_BOT_TOKEN, name="Dilbert")
 logger = logging.getLogger(__name__)
 
 
-@app.message(re.compile("^joke$"))  # type: ignore
+@app.message(re.compile("doi"))  # type: ignore
 def show_random_joke(message, say):
-    """Send a random pyjoke back"""
-    channel_type = message["channel_type"]
-    if channel_type != "im":
-        return
-
-    dm_channel = message["channel"]
-    user_id = message["user"]
-
-    joke = "Hello, World!"
-    logger.info(f"Sent joke < {joke} > to user {user_id}")
-
-    say(text=joke, channel=dm_channel)
+    # channel_type = message["channel_type"]
+    # channel = message["channel"]
+    blocks = message["blocks"]
+    for block in blocks:
+        for block_elements in block["elements"]:
+            for element in block_elements["elements"]:
+                if element["type"] == "link":
+                    print(element["url"])
 
 
 def main():
